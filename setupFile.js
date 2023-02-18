c354ec0b98fbdbbd00793ef6201a81d31fe41c96
Jenny Seburn <jennifer.seburn@gmail.com>
2023-02-18 13:33:22 -0500

    update deprecated method

--- setupFile.js
+++ setupFile.js
@@ -1,7 +1,7 @@
-import { setGlobalConfig } from "@storybook/testing-react";
+import { setProjectAnnotations } from "@storybook/testing-react";
 
 // Storybook's preview file location
 import * as globalStorybookConfig from "./.storybook/preview";
 
 // Replace with setProjectAnnotations if you are using the new pre-release version the addon
-setGlobalConfig(globalStorybookConfig);
+setProjectAnnotations(globalStorybookConfig);
